52d2fdac281e700ae1f100e8904a87dc3313693e
slay-code <slaymr300@gmail.com>
2024-11-06 21:12:08 +0300

    приложение posts переведён в cbv

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('add-post/', views.add_post, name='add_post'),
+    path('add-post/', views.AddPostView.as_view(), name='add_post'),
     path('delete-post/<int:game_id>/', views.del_post, name='del_post'),
-    path('my-post/', views.my_posts, name='my_posts'),
+    path('my-post/', views.MyPostsView.as_view(), name='my_posts'),
 ]
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,47 +1,76 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views.decorators.http import require_POST
+from django.views.generic import CreateView, DeleteView, ListView
+from django.db import connection
 
 from .forms import AddPostForm
 from mysite.models import CategoryGame, Game
 
 
-@login_required(login_url="users:anon")
-def add_post(request):
-    if request.method == 'POST':
-        form = AddPostForm(data=request.POST)
-        if form.is_valid():
-            avtor = form.save(commit=False)
-            avtor.avtor = request.user
-            avtor.save()
+# @login_required(login_url="users:anon")
+# def add_post(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(data=request.POST)
+#         if form.is_valid():
+#             avtor = form.save(commit=False)
+#             avtor.avtor = request.user
+#             avtor.save()
             
-            return redirect('mysite:home')
-    else:
-        form = AddPostForm()
+#             return redirect('mysite:home')
+#     else:
+#         form = AddPostForm()
             
-    context = {
-        'title': "Добавить пост",
-        'form': form,
-    }
+#     context = {
+#         'title': "Добавить пост",
+#         'form': form,
+#     }
     
-    return render(request, 'posts/add_post.html', context)
+#     return render(request, 'posts/add_post.html', context)
 
 
-@require_POST
+class AddPostView(LoginRequiredMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'posts/add_post.html'
+    extra_context = {'title': 'Создать пост'}
+    login_url = reverse_lazy('users:anon')
+    
+    def form_valid(self, form):
+        avtor = form.save(commit=False)
+        avtor.avtor = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self) -> str:
+        return reverse_lazy('mysite:home')
+
+
 def del_post(request, game_id):
-    post = Game.objects.get(id=game_id, avtor=request.user)
+    post = get_object_or_404(Game, id=game_id, avtor=request.user)
     post.delete()
     return redirect("mysite:home")
 
 
-login_required(login_url='users:login')
-def my_posts(request):
+# class DeletePostView(LoginRequiredMixin, DeleteView):
+
+
+# login_required(login_url='users:login')
+# def my_posts(request):
     
-    context = {
-        'posts': Game.objects.filter(avtor=request.user).select_related('category', 'avtor'),
-        'title': "Мои публикаци",
-    }
+#     context = {
+#         'posts': Game.objects.filter(avtor=request.user).select_related('category', 'avtor'),
+#         'title': "Мои публикаци",
+#     }
     
-    return render(request, 'posts/my_posts.html', context)
+#     return render(request, 'posts/my_posts.html', context)
     
+    
+class MyPostsView(LoginRequiredMixin, ListView):
+    template_name = 'posts/my_posts.html'
+    context_object_name = 'posts'
+    extra_context = {'title': "Мои публикаци"}
+    login_url = 'users:login'
+    
+    def get_queryset(self):
+        return Game.objects.filter(avtor=self.request.user).select_related('category', 'avtor')
